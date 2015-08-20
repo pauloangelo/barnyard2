518c9ab4cf4cd7f1589ff02def82efb231fb0587
Paulo Angelo <pa@pauloangelo.com>
2015-08-20 14:56:46 -0300

    non-functional version

--- src/plugbase.c
+++ src/plugbase.c
@@ -75,6 +75,7 @@
 #include "output-plugins/spo_echidna.h"
 #include "output-plugins/spo_sguil.h"
 #include "output-plugins/spo_syslog_full.h"
+#include "output-plugins/spo_hogzilla.h"
 
 extern InputConfigFuncNode  *input_config_funcs;
 extern OutputConfigFuncNode *output_config_funcs;
@@ -365,6 +366,8 @@ void RegisterOutputPlugins(void)
     SguilSetup();
 
     OpSyslog_Setup();
+
+    HogzillaSetup();
 
 }
 
